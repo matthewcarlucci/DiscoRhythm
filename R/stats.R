64dc0b14de27d4bfc458532ebdf2def2e75d2f1b
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-08-08 15:26:59 +0300

    made cosinor work with NA values present

--- R/stats.R
+++ R/stats.R
@@ -22,12 +22,61 @@
 #' @examples
 #' \dontrun{
 #' tmpData <- matrix(rnorm(24 * 1000), ncol = 24)
+#' tmpData[sample(length(tmpData), nrow(tmpData))] <- NA
 #' lmCSmat(tmpData, 1:24, 24)
 #' }
 #' @author Karolis Koncevičius
 #'
 #' @keywords internal
 lmCSmat <- function(x, zts, per = 24) {
+  Y <- as.matrix(x)
+
+  res <- lmCSmatNoNA(Y[NULL,], zts, per)
+  res[1:nrow(Y),] <- NA
+
+  obsinds  <- t(!is.na(Y))
+  obstypes <- unique(obsinds, MARGIN = 2)
+  obstypes <- obstypes[,colSums(obstypes) > 2]
+
+  for(i in seq_len(ncol(obstypes))) {
+    inds <- colMeans(obsinds == obstypes[,i]) == 1
+    res[inds,] <- lmCSmatNoNA(Y[inds,obstypes[,i],drop=FALSE], zts[obstypes[,i]], per)
+    print(i)
+  }
+
+  res
+}
+
+#' Cosinor Without NA Values
+#'
+#' Fixed period cosinor on each row of a matrix with no missing values.
+#'
+#' Fits a cosinor model to each row of a matrix that has no NA values.
+#'
+#' @param x    - numeric data matrix
+#' @param zts  - numeric vector of length ncol(data) representing time points
+#'               for each data column
+#' @param per  - period of oscillations (default=24)
+#'
+#' @return data frame with the following estimated statistics:
+#' \itemize{
+#'         \item acrophase - acrophases
+#'         \item amplitude - amplitudes
+#'         \item Rsq - r-squared values
+#'         \item pvalue - p-values
+#'         \item intercept - intercept coefficient
+#'         \item sincoef - sine coefficient
+#'         \item coscoef - cosine coefficient
+#'}
+#' @examples
+#' \dontrun{
+#' tmpData <- matrix(rnorm(24 * 1000), ncol = 24)
+#' lmCSmat(tmpData, 1:24, 24)
+#' }
+#' @author Karolis Koncevičius
+#'
+#' @keywords internal
+lmCSmatNoNA <- function(x, zts, per = 24) {
     Y <- as.matrix(x)
 
     x1 <- sin(2 * pi * zts / per)
